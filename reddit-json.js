e7d4da95751046f63f7f5b0a122c8be83ad68544
Silentz01 <158164186+Silentz01@users.noreply.github.com>
2025-06-18 22:28:43 +0100

    Update reddit-json.js

--- reddit-json.js
+++ reddit-json.js
@@ -1,10 +1,19 @@
-let obj = JSON.parse($response.body);
+let body = $response.body;
+let obj = JSON.parse(body);
 
-if (obj.data && obj.data.children) {
+if (obj.data && Array.isArray(obj.data.children)) {
   obj.data.children = obj.data.children.filter(item => {
-    return !(item.data && (item.data.isSponsored || item.data.promoted));
+    const d = item.data || {};
+
+    // Aggressive filtering of promoted/ads
+    const isAd = d.isSponsored ||
+                 d.promoted ||
+                 d.adserverClickUrl ||
+                 d.author === 'Promoted' ||
+                 d.name?.includes('t3_') && d.url_overridden_by_dest?.includes('utm_source=reddit');
+
+    return !isAd;
   });
 }
 
 $done({ body: JSON.stringify(obj) });
-
