8d642aa50847f2335b7ca90e5e1170713158cd3f
Silentz01 <158164186+Silentz01@users.noreply.github.com>
2025-06-19 14:03:01 +0100

    Update reddit-feed-adclean.js

--- reddit-feed-adclean.js
+++ reddit-feed-adclean.js
@@ -1,11 +1,11 @@
-// reddit-ad-filter.js
-let obj = JSON.parse($response.body);
+let body = $response.body;
+let obj = JSON.parse(body);
 
-// Clean out promoted (ad) posts
-if (obj?.data?.children && Array.isArray(obj.data.children)) {
-  obj.data.children = obj.data.children.filter(
-    i => !(i.data && i.data.is_promoted)
-  );
+// Filter promoted content (ads)
+if (obj?.data?.children?.length > 0) {
+  obj.data.children = obj.data.children.filter(item => {
+    return !(item?.data?.is_promoted === true || item?.data?.placement === 1);
+  });
 }
 
 $done({ body: JSON.stringify(obj) });
